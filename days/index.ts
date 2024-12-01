81d4144ca70cd8c92d5d8772cb6436e303cbf9bb
Dominic Drees <dominic.drees@atino.de>
2024-12-02 00:28:46 +0100

    mark day 1 part 2 as solved, add day 2 template

--- days/index.ts
+++ days/index.ts
@@ -2,6 +2,7 @@ export type Challenge = {key: string, star: 0 | 1 | 2, solve: (print: (s: strin
 
 export const Challenges: Challenge[] = [
 
-    {key: "1", star: 1, solve: require("./day1/solve").solve},
+    {key: "1", star: 2, solve: require("./day1/solve").solve},
+	//{key: "2", star: 0, solve: require("./day2/solve").solve},
 
 ]
